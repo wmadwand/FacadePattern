9632ac2efb36af96b363dc303f51e172cd976ed9
wmadwand <wmadwand@gmail.com>
2018-09-15 14:51:15 +0300

    One nice primer

--- Program.cs
+++ Program.cs
@@ -2,11 +2,90 @@
 
 namespace Facade
 {
+    class TextEditor
+    {
+        public void CreateCode()
+        {
+            Console.WriteLine("Написание кода");
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Сохранение кода");
+        }
+    }
+
+    class Compiller
+    {
+        public void Compile()
+        {
+            Console.WriteLine("Компиляция приложения");
+        }
+    }
+
+    class CLR
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Выполнение приложения");
+        }
+
+        public void Finish()
+        {
+            Console.WriteLine("Завершение работы приложения");
+        }
+    }
+
+    class VisualStudioFacade
+    {
+        TextEditor textEditor;
+        Compiller compiller;
+        CLR clr;
+
+        public VisualStudioFacade(TextEditor te, Compiller compil, CLR clr)
+        {
+            this.textEditor = te;
+            this.compiller = compil;
+            this.clr = clr;
+        }
+
+        public void Start()
+        {
+            textEditor.CreateCode();
+            textEditor.Save();
+            compiller.Compile();
+            clr.Execute();
+        }
+
+        public void Stop()
+        {
+            clr.Finish();
+        }
+    }
+
+    class Programmer
+    {
+        public void CreateApplication(VisualStudioFacade facade)
+        {
+            facade.Start();
+            facade.Stop();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TextEditor textEditor = new TextEditor();
+            Compiller compiller = new Compiller();
+            CLR clr = new CLR();
+
+            VisualStudioFacade ide = new VisualStudioFacade(textEditor, compiller, clr);
+
+            Programmer programmer = new Programmer();
+            programmer.CreateApplication(ide);
+
+            Console.Read();
         }
     }
 }
